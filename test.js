3a5ee3bc47e56871dd1acda2889acb7d17ad2f20
GuoChenLin <741356285@qq.com>
2020-03-27 14:42:45 +0800

    版本回退

--- test.js
+++ test.js
@@ -6,6 +6,6 @@ $(function () {
     $('#app').html('hello')
     // 下面是其他人修改的内容
     $('#app').on('click',function() {
-      consolg.log($(this).html())
+      console.log($(this).html())
     })
 })
